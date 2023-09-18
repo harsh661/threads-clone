0eb123ae9b35a4af735a3c7d28c58eac5375609a
harsh661 <plasmaharsh@gmail.com>
2023-09-18 21:28:08 +0530

    feat: redirect user to about form

---
+++ app/(auth)/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section>
+        <div>
+            <h2>Tell us more about you</h2>
+        </div>
+    </section>
+  )
+}
+
+export default About
